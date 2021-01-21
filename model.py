a6c64de63afe2a82008cecd62decedcd9604452a
ayeffkay <chatton1@mail.ru>
2021-01-21 11:38:05 +0300

    Toy pytorch model

---
+++ model.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class FFN(nn.Module):
+    def __init__(self, input_size):
+         super().__init__()
+         self.l1 = nn.Linear(input_size, input_size * 3)
+         self.l2 = nn.Linear(input_size * 3, input_size * 2)
+         self.l3 = nn.Linear(input_size * 2, 1)
+
+
+    def forward(self, inputs):
+        o1 = F.relu(self.l1(inputs))
+        o2 = torch.tanh(self.l2(o1))
+        o3 = self.l3(o2)
+        return o3
